d2475433997c9bd815a575dd74dd9e8b22bdedbb
Jean Wang <weizi.wang75@gmail.com>
2023-09-12 16:28:22 +0800

    update dist folder

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  publicPath: '/dist/',
+  publicPath: '/new_prj22/dist/',
 })
 
 // module.exports = {
