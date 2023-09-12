4662dcead0042aaf7dd5ef500cb95baeadc1a135
Jean Wang <weizi.wang75@gmail.com>
2023-09-12 15:06:35 +0800

    0912#3 編譯完成

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,19 @@
   <h2> app.vue </h2>
   <hr>
   <router-view/>
+  <br>
+  <p>env</p>
+  {{ name }}
 </template>
-
+<script>
+export default {
+  data() {
+    return {
+      name: process.env.VUE_APP_NAME,
+    }
+  }
+}
+</script>
 <style lang="scss">
 @import "bootstrap";
 
